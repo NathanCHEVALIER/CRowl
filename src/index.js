d844e3f7c1d4a202676b431b2fa5241a15d10af2
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2022-09-13 14:46:51 +0200

    [DomEnum]: Add: Subdomain enumeration with cert.sh

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const program = require('commander')
 
 program.version('0.1').description('CRowl')
 
-program.command('find').description('find sudomains')
-  .option('-d, --domain <domain>', 'domain name')
-  .action(async (options) => {
-    subdomain.crawl(options.domain);
-  })
+program
+    .command('find')
+    .description('find sudomains')
+    .option('-d, --domain <domain>', 'domain name')
+    .action(async (options) => {
+        subdomain.crawl(options.domain);
+    });
 
-program.parse(process.argv)
+program.parse(process.argv);
--- src/subdomainsEnumerate/index.js
+++ src/subdomainsEnumerate/index.js
@@ -1,5 +1,21 @@
-const crawl = (url) => {
-    console.log(url);
+const request = require('./../request');
+
+const crawl = async (url) => {
+    let path = 'https://crt.sh/?q=%.' + url + '&output=json';
+
+    const {body, response} = await request.getRequest(path);
+    const subdomains = JSON.parse(body);
+
+    console.log(subdomains);
+
+    subdomains.map( (elt) => {
+        return {
+            issuer: elt.issuer_name,
+            name: elt.name_value
+        };
+    }).forEach( (elt) => { 
+        console.log(elt);
+    });
 }
 
 module.exports = {
