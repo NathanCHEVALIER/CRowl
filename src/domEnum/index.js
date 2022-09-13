af3f437d3f39029820b1df99ec1f8b69ec21eb5a
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2022-09-13 18:10:44 +0200

    [domEnum]: Update: more Api

--- src/domEnum/index.js
+++ src/domEnum/index.js
@@ -1,14 +1,27 @@
 const request = require('./../request');
 
+const cleanUrl = (url) => {
+    if (url === undefined)
+        return;
+
+    return url
+        .replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace('*.', '')
+        .split('/')[0];
+}
+
 const findCrt = async (url) => {
     let domains = {};
     let apis = 'https://crt.sh/?q=%.' + url + '&output=json';
 
-    const {body, response} = await request.getRequest(apis);
+    const {body, response} = await request.getRequest(apis).catch();
     let subdomains = JSON.parse(body);
 
     subdomains.forEach( (elt) => {
         elt.name_value.split('\n').forEach( (s) => {
+            s = cleanUrl(s);
             domains[s] = {
                 issuer: elt.issuer_name
             };
@@ -33,8 +46,7 @@ const findWebArchive = async (url) => {
         return (typeof(elt) === 'string' && elt !== 'original');
     }).map((elt) => {
         console.log(elt);
-        let a = elt.replace('https://', '').replace('http://', '').replace('www.', '').split('/');
-        return a[0];
+        return cleanUrl(elt);
     });
 
     subdomains.forEach( (elt) => { 
@@ -45,9 +57,51 @@ const findWebArchive = async (url) => {
     return domains;
 }
 
+const findAlienVault = async (url) => {
+    let domains = {};
+    let apis = 'https://otx.alienvault.com/api/v1/indicators/domain/' + url + '/passive_dns';
+
+    const {body, response} = await request.getRequest(apis);
+    let subdomains = JSON.parse(body);
+
+    subdomains = subdomains["passive_dns"].map((elt) => {
+        //console.log(elt.hostname);
+        return cleanUrl(elt.hostname);
+    });
+
+    subdomains.forEach( (elt) => {
+        console.log(elt);
+        domains[elt] = {
+        };
+    });
+
+    return domains;
+}
+
+const findCertSpotter = async (url) => {
+    let domains = {};
+    let apis = 'https://api.certspotter.com/v1/issuances?domain=' + url + '&include_subdomains=true&expand=dns_names&expand=issuer&expand=cert';
+
+    const {body, response} = await request.getRequest(apis);
+    let subdomains = JSON.parse(body);
+
+    subdomains = subdomains.map((elt) => {
+        //console.log(elt.hostname);
+        return cleanUrl(elt.dns_names[0]);
+    });
+
+    subdomains.forEach( (elt) => {
+        console.log(elt);
+        domains[elt] = {
+        };
+    });
+
+    return domains;
+}
+
 const find = async (url) => {
     let domains = {};
-    const crt = await findCrt(url);
+   /* const crt = await findCrt(url);
     console.log(crt);
 
     let count = 0;
@@ -60,7 +114,7 @@ const find = async (url) => {
 
     console.log("============== [ CRT.sh ]: " + count);
 
-    const webArchive = await findWebArchive(url).catch((error) => {
+    /*const webArchive = await findWebArchive(url).catch((error) => {
         console.log(error);
     });
 
@@ -72,6 +126,36 @@ const find = async (url) => {
     });
 
     console.log("============== [ WebArchive ]: " + count);
+*/
+    const alienVault = await findAlienVault(url).catch((error) => {
+        console.log(error);
+    });
+
+    count = 0;
+    Object.keys(alienVault).forEach((key) => {
+        if (domains[key] === undefined) {
+            console.log(key);
+            domains[key] = alienVault[key]; count++;
+        }
+    });
+
+    console.log("============== [ AlienVault ]: " + count);
+
+    const certspotter = await findCertSpotter(url).catch((error) => {
+        console.log(error);
+    });
+
+    count = 0;
+    Object.keys(certspotter).forEach((key) => {
+        if (domains[key] === undefined) {
+            console.log(key);
+            domains[key] = certspotter[key]; 
+            count++;
+        }
+    });
+
+    console.log("============== [ CertSpotter ]: " + count);
+
 
     return Object.keys(domains);
 }
