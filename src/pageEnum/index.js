d2b77a03cc0bebba7d2b679c5c7a230c7c7e5129
Alan GUERET <agueret@stroople.com>
2022-09-15 16:57:52 +0000

    [pageEnum] Start - working on the crawler

--- src/pageEnum/index.js
+++ src/pageEnum/index.js
@@ -1,8 +1,10 @@
 const request = require('./../request');
 const interface = require('./../interface');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer-core');
 
-urls = [];
+urls = new Set();
+ignoredExtension = ["js", "jpg", "png", "gif", "bmp", "svg", "css"];
 
 const cleanUrl = (url) => {
     if (url === undefined || url === null)
@@ -15,10 +17,20 @@ const cleanUrl = (url) => {
         .replace('*.', '');
 }
 
+const checkExtension = (url) => {
+    if (url === undefined || url === null)
+        return false;
+
+    let splittedURL = url.split(".");
+    let extension = splittedURL[splittedURL.length - 1];
+
+    return ignoredExtension.includes(extension);
+}
+
 const fromSiteMap = (domain) => { return new Promise(async (resolve, reject) => {
-    let tmpUrls = [];
+    let tmpUrls = new Set();
     const response = await request
-        .getRequest("https://" + domain + "/sitemap.xml", 50000)
+        .getRequest("https://" + domain + "/sitemap.xml")
         .catch((err) => {
             resolve(tmpUrls);
             return;
@@ -38,13 +50,42 @@ const fromSiteMap = (domain) => { return new Promise(async (resolve, reject) => 
     urlList.each((i, elt) => {
         let u = $(elt).text();
         if (u !== 'None') {
-            tmpUrls.push(cleanUrl(u));
+            tmpUrls.add(cleanUrl(u));
         }
-        // if (cleanUrl(u) === "de-memoire-vive-philippe-dewost.epita.fr/post-sitemap.xml") {
-        //     console.log(domain);
-        // }
     });
-    // console.log(tmpUrls);
+    resolve(tmpUrls);
+})};
+
+const Crawl = (domain, page) => { return new Promise(async (resolve, reject) => {
+    let tmpUrls = new Set();
+    const response = await request
+        .getRequest("https://" + page)
+        .catch((err) => {
+            resolve(tmpUrls);
+            return;
+        });
+
+    if (response === undefined) {
+        resolve(tmpUrls);
+        return;
+    }
+
+    let {body, header} = response;
+
+    let u;
+    let regexUrl = new RegExp('https?:\/\/[-\._a-zA-Z0-9]*' + domain + '[-\.\/_a-zA-Z0-9=+?]*', 'gm');
+
+    while ((u = regexUrl.exec(body)) !== null) {
+        if (checkExtension(u[0]) || tmpUrls.has(u[0])) {
+            continue;
+        }
+        tmpUrls.add(u[0]);
+    }
+
+    // console.log(header);
+    // console.log(body);
+    //const $ = cheerio.load(body);
+
     resolve(tmpUrls);
 })};
 
@@ -54,8 +95,12 @@ const getPages = (domains) => { return new Promise(async (resolve, reject) => {
         const spinner = interface.waitLog('Looking for pages in ' + domain);
         promises.push(fromSiteMap(domain)
             .then((res) => {
-                spinner.succeed(domain + ' is finished: ' + res.length);
-                urls = urls.concat(res)
+                if (res.size === 0) {
+                    spinner.warn(domain + ': no pages found');
+                } else {
+                    spinner.succeed(domain + ' is finished: ' + res.size);
+                    urls = new Set([...urls, ...res]);
+                }
             })
         );
     }
@@ -63,12 +108,13 @@ const getPages = (domains) => { return new Promise(async (resolve, reject) => {
     for (const promise of promises) {
         await promise;
     }
-    console.log(urls);
     resolve(urls)
 })};
 
 // getPages(["lcl.fr", "epita.fr", "labanquepostale.fr", "auchan.fr"])
 //     .then((res) => console.log(res));
+
+// Crawl("owasp.org", "owasp.org/www-project-top-ten/").then((val) => console.log(val));
 
 module.exports = {
     getPages
